b4bcacb9ff78bb77908b594d46caa9da9b271d02
lowzhao <lowzhao@gmail.com>
2019-12-02 22:19:04 +0800

    First Main Commit.

---
+++ src/main/Main.java
@@ -0,0 +1,24 @@
+package main;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	public static void main(String[] args) {
+		System.out.println("Hello Galen.");
+		launch(args); // setup app as java application;
+		
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
