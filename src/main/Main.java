81d88dd4721d6b428de280b7a20c5104114f8e4b
lowzhao <lowzhao@gmail.com>
2019-12-03 03:13:54 +0800

    JavaFX Learning: creating Scene, Layout and Button.

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,6 +9,9 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	
+	Button button;
+	
 	public static void main(String[] args) {
 		System.out.println("Hello Galen.");
 		launch(args); // setup app as java application;
@@ -17,7 +20,23 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+		primaryStage.setTitle("Leave System.");
+		
+		// Stage -> Scene -> Layout -> Buttons, blahblah.....
+		
+		// creating layout
+		
+		
+		button = new Button();
+		button.setText("Click me.");
+		
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		
+		Scene scene = new Scene(layout, 1000, 1000);
+		primaryStage.setScene(scene);
+		
+		primaryStage.show();
 		
 	}
 	
